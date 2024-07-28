d6d49e54b80a07e0a8b20f4e15bbddd5d62e90ff
andrearita <andrea_rita@DESKTOP-0VLHD6A.localdomain>
2024-07-28 15:33:17 +0800

    First commit for task0_1, task0_2

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -107,16 +107,18 @@ def test_sigmoid(a: float) -> None:
     * It crosses 0 at 0.5
     * It is  strictly increasing.
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert 0 <= sigmoid(a) <= 1
+    assert_close(1 - sigmoid(a), sigmoid(-a))
+    assert_close(sigmoid(0), 0.5)
+    assert sigmoid(a) <= sigmoid(a + 1)
 
 
 @pytest.mark.task0_2
 @given(small_floats, small_floats, small_floats)
 def test_transitive(a: float, b: float, c: float) -> None:
     "Test the transitive property of less-than (a < b and b < c implies a < c)"
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    if a < b and b < c:
+        assert a < c
 
 
 @pytest.mark.task0_2
@@ -125,8 +127,7 @@ def test_symmetric() -> None:
     Write a test that ensures that :func:`minitorch.operators.mul` is symmetric, i.e.
     gives the same value regardless of the order of its input.
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert_close(mul(1, 2), mul(2, 1))
 
 
 @pytest.mark.task0_2
@@ -135,8 +136,9 @@ def test_distribute() -> None:
     Write a test that ensures that your operators distribute, i.e.
     :math:`z \times (x + y) = z \times x + z \times y`
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    a = 1
+    b = 2
+    assert_close(mul(a, add(b, b)), add(mul(a, b), mul(a, b)))
 
 
 @pytest.mark.task0_2
@@ -144,8 +146,11 @@ def test_other() -> None:
     """
     Write a test that ensures some other property holds for your functions.
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    a = 1
+    b = 2
+    # associative property
+    assert_close(add(add(a, b), b), add(a, add(b, b)))
+    assert_close(mul(mul(a, b), b), mul(a, mul(b, b)))
 
 
 # ## Task 0.3  - Higher-order functions
